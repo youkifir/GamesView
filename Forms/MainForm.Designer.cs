0a43a8a8b20882f7171e217af35b978f3c20b195
Youkifir <ridertop2009@gmail.com>
2025-12-03 19:29:52 +0200

    MainForm from Design

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace GamesView.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btnProfile = new Button();
+            btnNews = new Button();
+            btnReview = new Button();
+            btnFavorite = new Button();
+            btnLibrary = new Button();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(tableLayoutPanel1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1127, 60);
+            panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 5;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Controls.Add(btnProfile, 4, 0);
+            tableLayoutPanel1.Controls.Add(btnNews, 3, 0);
+            tableLayoutPanel1.Controls.Add(btnReview, 2, 0);
+            tableLayoutPanel1.Controls.Add(btnFavorite, 1, 0);
+            tableLayoutPanel1.Controls.Add(btnLibrary, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(1127, 60);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnProfile
+            // 
+            btnProfile.Dock = DockStyle.Fill;
+            btnProfile.FlatAppearance.BorderColor = Color.FromArgb(63, 81, 181);
+            btnProfile.FlatAppearance.MouseOverBackColor = Color.Red;
+            btnProfile.FlatStyle = FlatStyle.Flat;
+            btnProfile.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnProfile.ForeColor = Color.WhiteSmoke;
+            btnProfile.Location = new Point(903, 3);
+            btnProfile.Name = "btnProfile";
+            btnProfile.Size = new Size(221, 54);
+            btnProfile.TabIndex = 4;
+            btnProfile.Text = "Профіль";
+            btnProfile.UseVisualStyleBackColor = true;
+            btnProfile.Click += btnProfile_Click;
+            // 
+            // btnNews
+            // 
+            btnNews.Dock = DockStyle.Fill;
+            btnNews.FlatAppearance.BorderColor = Color.FromArgb(63, 81, 181);
+            btnNews.FlatAppearance.MouseOverBackColor = Color.Red;
+            btnNews.FlatStyle = FlatStyle.Flat;
+            btnNews.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnNews.ForeColor = Color.WhiteSmoke;
+            btnNews.Location = new Point(678, 3);
+            btnNews.Name = "btnNews";
+            btnNews.Size = new Size(219, 54);
+            btnNews.TabIndex = 3;
+            btnNews.Text = "Додати новини";
+            btnNews.UseVisualStyleBackColor = true;
+            btnNews.Click += btnNews_Click;
+            // 
+            // btnReview
+            // 
+            btnReview.Dock = DockStyle.Fill;
+            btnReview.FlatAppearance.BorderColor = Color.FromArgb(63, 81, 181);
+            btnReview.FlatAppearance.MouseOverBackColor = Color.Red;
+            btnReview.FlatStyle = FlatStyle.Flat;
+            btnReview.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnReview.ForeColor = Color.WhiteSmoke;
+            btnReview.Location = new Point(453, 3);
+            btnReview.Name = "btnReview";
+            btnReview.Size = new Size(219, 54);
+            btnReview.TabIndex = 2;
+            btnReview.Text = "Відгуки";
+            btnReview.UseVisualStyleBackColor = true;
+            btnReview.Click += btnReview_Click;
+            // 
+            // btnFavorite
+            // 
+            btnFavorite.Dock = DockStyle.Fill;
+            btnFavorite.FlatAppearance.BorderColor = Color.FromArgb(63, 81, 181);
+            btnFavorite.FlatAppearance.MouseOverBackColor = Color.Red;
+            btnFavorite.FlatStyle = FlatStyle.Flat;
+            btnFavorite.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnFavorite.ForeColor = Color.WhiteSmoke;
+            btnFavorite.Location = new Point(228, 3);
+            btnFavorite.Name = "btnFavorite";
+            btnFavorite.Size = new Size(219, 54);
+            btnFavorite.TabIndex = 1;
+            btnFavorite.Text = "Улюблені ігри";
+            btnFavorite.UseVisualStyleBackColor = true;
+            btnFavorite.Click += btnFavorite_Click;
+            // 
+            // btnLibrary
+            // 
+            btnLibrary.Dock = DockStyle.Fill;
+            btnLibrary.FlatAppearance.BorderColor = Color.FromArgb(63, 81, 181);
+            btnLibrary.FlatAppearance.MouseOverBackColor = Color.Red;
+            btnLibrary.FlatStyle = FlatStyle.Flat;
+            btnLibrary.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnLibrary.ForeColor = Color.WhiteSmoke;
+            btnLibrary.Location = new Point(3, 3);
+            btnLibrary.Name = "btnLibrary";
+            btnLibrary.Size = new Size(219, 54);
+            btnLibrary.TabIndex = 0;
+            btnLibrary.Text = "Бібліотека";
+            btnLibrary.UseVisualStyleBackColor = true;
+            btnLibrary.Click += btnLibrary_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel3);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 60);
+            panel2.Margin = new Padding(10);
+            panel2.Name = "panel2";
+            panel2.Padding = new Padding(16);
+            panel2.Size = new Size(1127, 616);
+            panel2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(45, 45, 45);
+            panel4.Controls.Add(button2);
+            panel4.Controls.Add(textBox2);
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(comboBox1);
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(textBox1);
+            panel4.Controls.Add(label2);
+            panel4.Controls.Add(button1);
+            panel4.Controls.Add(pictureBox1);
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(16, 66);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(1095, 534);
+            panel4.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button2.BackColor = Color.Lime;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(914, 242);
+            button2.Name = "button2";
+            button2.Size = new Size(164, 39);
+            button2.TabIndex = 10;
+            button2.Text = "Додати гру";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(543, 196);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(535, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(428, 195);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 25);
+            label4.TabIndex = 6;
+            label4.Text = "Опис гри :";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Хоррор", "Одиночна гра", "Інтерактивне кіно" });
+            comboBox1.Location = new Point(543, 121);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(535, 28);
+            comboBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(428, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(106, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Категорія :";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(543, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(535, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(428, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Назва гри :";
+            label2.Click += label2_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(3, 6);
+            button1.Name = "button1";
+            button1.Size = new Size(403, 35);
+            button1.TabIndex = 1;
+            button1.Text = "Обрати фото";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.FromArgb(50, 50, 50);
+            pictureBox1.Location = new Point(3, 53);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(403, 449);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label1);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(16, 16);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1095, 50);
+            panel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(833, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(259, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Додати нову гру";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(30, 30, 30);
+            ClientSize = new Size(1127, 676);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "MainForm";
+            Text = "MainForm";
+            panel1.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btnLibrary;
+        private Button btnProfile;
+        private Button btnNews;
+        private Button btnReview;
+        private Button btnFavorite;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label1;
+        private Panel panel4;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label4;
+        private ComboBox comboBox1;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button2;
+    }
+}
---
+++ Forms/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamesView.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLibrary_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFavorite_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReview_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNews_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProfile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelBanner_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelNews_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowNews_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FlowLayoutPanelRecommendations_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
